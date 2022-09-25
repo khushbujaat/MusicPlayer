764daa8d0760624a94550f949191d3f11425bc22
unknown <jaatkhushbu71@gmail.com>
2022-09-25 21:18:09 +0530

    Initial commit

---
+++ music-list.js
@@ -0,0 +1,33 @@
+
+let allMusic = [
+    {
+        name: "cartoon - on&on",
+        artist: "cartoon",
+        img: "on&on",
+        src: "on&on"
+    },
+    {
+        name: "ed sheeran - shape of you",
+        artist: "ed sheeran",
+        img: "shapeofyou",
+        src: "shapeofyou"
+    },
+    {
+        name: "Ikson Anywhere - Ikson",
+        artist: "jordan Schor",
+        img: "anywhere",
+        src: "anywhere"
+    },
+    {
+        name: "Beauz & Jvna - crazy",
+        artist: "Beauz & Jvna",
+        img: "crazy",
+        src: "crazy"
+    },
+    {
+        name: "Jim - Sun Goes Down",
+        artist: "Jim Yosef x Roy",
+        img: "sungoesdown",
+        src: "sungoesdown"
+    },
+];
